a0924aa9bf8af2c9b7f5173682c4f488b3eed7b9
GC <gareth2.chappell@live.uwe.ac.uk>
2025-05-07 14:33:06 +0100

    Updated implementation of encryption and decryption functionality.

--- src/Networking/Decryption.cs
+++ src/Networking/Decryption.cs
@@ -5,18 +5,27 @@ interface IDecryption
 }
 class AESDecryption : IDecryption
 {
-    public Task Decrypt(Stream input, Stream output, SymmetricAlgorithm algorithm, CancellationToken token = default)
+    public async Task Decrypt(Stream input, Stream output, SymmetricAlgorithm algorithm, CancellationToken token = default)
     {
         if (input == null) {throw new ArgumentNullException(nameof(input));}
         if (output == null) {throw new ArgumentNullException(nameof(output));}
         if (algorithm == null) {throw new ArgumentNullException(nameof(algorithm));}
         try
         {
-
+            using CryptoStream cryptoStream = new CryptoStream(input, algorithm.CreateDecryptor(), CryptoStreamMode.Read);
+            await cryptoStream.CopyToAsync(output, bufferSize: 81920, cancellationToken: token);
         }
-        catch ()
+        catch (CryptographicException ex)
         {
-            
+            throw new InvalidOperationException("Decryption failed due to cryptographical error.", ex);
+        }
+        catch (IOException ex)
+        {
+            throw new InvalidOperationException("Decryption failed due to I/O error.", ex);
+        }
+        catch (OperationCanceledException ex)
+        {
+            throw new TaskCanceledException("Decryption task was cancelled.", ex);
         }
     }
 }
--- src/Networking/Encryption.cs
+++ src/Networking/Encryption.cs
@@ -5,18 +5,27 @@ interface IEncryption
 }
 class AESEncryption : IEncryption
 {
-    public Task Encrypt(Stream input, Stream output, SymmetricAlgorithm algorithm, CancellationToken token = default)
+    public async Task Encrypt(Stream input, Stream output, SymmetricAlgorithm algorithm, CancellationToken token = default)
     {
         if (input == null) {throw new ArgumentNullException(nameof(input));}
         if (output == null) {throw new ArgumentNullException(nameof(output));}
         if (algorithm == null) {throw new ArgumentNullException(nameof(algorithm));}
         try
         {
-            
+            using CryptoStream cryptoStream = new CryptoStream(input, algorithm.CreateEncryptor(), CryptoStreamMode.Write);
+            await cryptoStream.CopyToAsync(output, bufferSize: 81920, cancellationToken: token);
         }
-        catch ()
+        catch (CryptographicException ex)
         {
-
+            throw new InvalidOperationException("Encryption failed due to cryptographical error.", ex);
+        }
+        catch (IOException ex)
+        {
+            throw new InvalidOperationException("Encryption failed due to I/O error.", ex);
+        }
+        catch (OperationCanceledException ex)
+        {
+            throw new TaskCanceledException("Encryption task was cancelled.", ex);
         }
     }
 }
